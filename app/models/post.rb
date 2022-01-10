8df07cc9351da2fb66e7056f5baf847f3eaa921c
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2022-01-10 13:35:57 -0500

    commit tree fd6644e2ce6d8690ef69ecab7452254c513acaf8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    
+    # validate :clickbait
+
+    # CLICKBAIT = [
+
+    # ]
+
+    # def clickbait
+    #     unless title.match?("You Won't Believe These True Facts")
+    #       errors.add(:title, "this is definitely clickbait")
+    #     end
+    # end
 end
